f8c049c742ddb50eb033db83d074c8316234037b
revathi <liai.revathikv@gmail.com>
2023-08-01 21:01:48 +0530

    code from live

--- application/views/home/hyperpay.php
+++
@@ -1,2 +0,0 @@
-<form action="<?php echo base_url() ; ?>home/paymentstatus/" class="paymentWidgets" data-brands="VISA MASTER AMEX"></form>
-<script src="https://test.oppwa.com/v1/paymentWidgets.js?checkoutId=<?php echo $checkoutId ?>"></script>
